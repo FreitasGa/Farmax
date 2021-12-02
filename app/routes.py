36fd7c2b4c05edc01c7b198b59c92c580d4dda70
FreitasGa <freitasga.01@gmail.com>
2021-12-02 02:39:44 -0300

    Now page redirects when executing a post action

--- app/routes.py
+++ app/routes.py
@@ -4,22 +4,25 @@ from config import mongo
 router = Blueprint("router", __name__)
 
 
-@router.route("/", methods=["GET", "POST"])
+@router.route("/", methods=["GET"])
 def main():
     medicine_collection = mongo.db.medicine
 
     items = medicine_collection.find()
     items = [item for item in items]
 
-    print(items, len(items))
-
-    if request.method == "POST":
-        name = request.form.get("Nome")
-        description = request.form.get("Descricao")
-        price = request.form.get("Preco")
-
-        item = {"name": name, "price": price, "description": description}
-        medicine_collection.insert_one(item)
-        print(item)
-
     return render_template("main.html", items=items, lenght=len(items))
+
+
+@router.route("/add_medicine", methods=["POST"])
+def add_medicine():
+    medicine_collection = mongo.db.medicine
+
+    name = request.form.get("Nome")
+    description = request.form.get("Descricao")
+    price = request.form.get("Preco")
+
+    item = {"name": name, "price": price, "description": description}
+    medicine_collection.insert_one(item)
+
+    return redirect(url_for("router.main"))
