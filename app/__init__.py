81a1399a5b2664eda670da1da9bb87ec588bc59c
FreitasGa <freitasga.01@gmail.com>
2021-12-02 02:30:02 -0300

    Saving data do database

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from config import mongo
 def create_app():
     app = Flask(__name__)
     app.config['MONGO_URI'] = 'mongodb+srv://admin:6iKyAiPFT9JAabB8@cluster0.9ck3e.mongodb.net/mydb?retryWrites=true' \
-                              '&w=majority '
+                              '&w=majority'
 
     mongo.init_app(app)
 
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,25 @@
 from flask import Blueprint, redirect, render_template, request, url_for
 from config import mongo
 
-router = Blueprint("main", __name__)
+router = Blueprint("router", __name__)
 
 
 @router.route("/", methods=["GET", "POST"])
 def main():
-    items = mongo.db.drugs.find()
+    medicine_collection = mongo.db.medicine
+
+    items = medicine_collection.find()
     items = [item for item in items]
 
     print(items, len(items))
 
     if request.method == "POST":
-        print('Post')
+        name = request.form.get("Nome")
+        description = request.form.get("Descricao")
+        price = request.form.get("Preco")
+
+        item = {"name": name, "price": price, "description": description}
+        medicine_collection.insert_one(item)
+        print(item)
 
     return render_template("main.html", items=items, lenght=len(items))
