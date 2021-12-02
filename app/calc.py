9f6188519730a8edf62166e39cd89721e6cf0ef6
FreitasGa <freitasga.01@gmail.com>
2021-12-02 00:57:20 -0300

    formmating files

--- app/calc.py
+++ app/calc.py
@@ -1,18 +1,19 @@
 from sympy import *
 
-def calcPreço(preco):
 
-    x = symbols('x') #atribuir a variavel x como sendo uma variavel matematica da funcao
-    c = 50000 + 80*x + 0.003*x**2 # funcao custo genérica
-    r = preco * x # renda
+def calcPreço(preco):
+    # atribuir a variavel x como sendo uma variavel matematica da funcao
+    x = symbols('x')
+    c = 50000 + 80*x + 0.003*x**2  # funcao custo genérica
+    r = preco * x  # renda
     l = r - c
 
-    init_printing(use_unicode=True) # iniciando o sympy, n sei se é realmente necessario
-    d1 = diff(l, x) # Derivada da função custo
+    # iniciando o sympy, n sei se é realmente necessario
+    init_printing(use_unicode=True)
+    d1 = diff(l, x)  # Derivada da função custo
 
-    max = solve(d1, x) # resolvendo a equação
-    max =  max[0] #Pegando
+    max = solve(d1, x)  # resolvendo a equação
+    max = max[0]  # Pegando
     max = Integer(max)
 
-    return max;
-
+    return max
