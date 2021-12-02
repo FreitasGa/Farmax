b4d45df5a6828fe7b7f5c41b92699f4136064a4b
lucasxxi <lucasleal2001@gmail.com>
2021-12-01 22:53:40 -0300

    função com descrições acerca da função de máximo

--- app/calc.py
+++ app/calc.py
@@ -1,17 +1,17 @@
 from sympy import *
 
-def calcPreço(preco):
+def calcPrecoMax(preco):
 
     x = symbols('x') #atribuir a variavel x como sendo uma variavel matematica da funcao
     c = 50000 + 80*x + 0.003*x**2 # funcao custo genérica
-    r = preco * x # renda
-    l = r - c
+    r = preco * x # função receita
+    l = r - c #lucro
 
-    init_printing(use_unicode=True) # iniciando o sympy, n sei se é realmente necessario
+    init_printing(use_unicode=True) # iniciando o sympy
     d1 = diff(l, x) # Derivada da função custo
 
     max = solve(d1, x) # resolvendo a equação
-    max =  max[0] #Pegando
+    max =  max[0] # Pegando a raiz positiva da funcao
     max = Integer(max)
 
     return max;
